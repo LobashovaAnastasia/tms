3471acb92a7a2f74c85e98d3cc44a395a3d16f7c
LobashovaAnastasia <anastasia.lobashova27@gmail.com>
2023-05-18 17:18:52 +0300

    Lesson2

---
+++ Lesson2/main.py
@@ -0,0 +1,10 @@
+# СРЕЗЫ
+my_string = "aaaaaBccBddBeeBffBggB"
+print(my_string[5::3])
+
+my_string2 = "AAAABBBBCCCCDDDDFFFF"
+print(my_string2[:4] + my_string2[4:8] + my_string2[8:12] + my_string2[12:16] + my_string2[16:20])
+
+my_string3 = "PYTHON"
+print(my_string3[::-1])
+
