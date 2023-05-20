5301055c8418e2afdd1de77c026a6ff7f50a49d0
LobashovaAnastasia <anastasia.lobashova27@gmail.com>
2023-05-20 17:39:02 +0300

    Lesson1

---
+++ Lesson1/homework1_1.py
@@ -0,0 +1,9 @@
+a = int(input('Введите значение a: '))
+b = int(input('Введите значение b: '))
+
+print(f'a + b = {a + b}')
+print(f'a - b = {a - b}')
+print(f'a * b = {a * b}')
+print(f'a**b = {a**b}')
+print(f'a % b = {a % b}')
+print(f'a // b = {a // b}')
---
+++ Lesson1/homework1_3.py
@@ -0,0 +1,4 @@
+x = int(input('Введите значение x: '))
+y = int(input('Введите значение y: '))
+
+print((abs(x) - abs(y))/(1 + abs(x * y)))
