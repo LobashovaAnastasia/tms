98f842e32dd308d6557ab567f3fe75522aa17c84
LobashovaAnastasia <anastasia.lobashova27@gmail.com>
2023-05-20 05:56:52 +0300

    Lesson2

---
+++ Lesson2/homework2_1.py
@@ -0,0 +1,5 @@
+var_1 = 5
+var_2 = 5
+var_3 = 5
+
+print(id(var_1), id(var_2), id(var_3))
--- Lesson2/main.py
+++ Lesson2/main.py
@@ -1,14 +1,3 @@
-# СРЕЗЫ
-my_string = "aaaaaBccBddBeeBffBggB"
-print(my_string[5::3])
-
-my_string2 = "AAAABBBBCCCCDDDDFFFF"
-print(my_string2[:4] + my_string2[4:8] + my_string2[8:12] + my_string2[12:16] + my_string2[16:20])
-
-my_string3 = "PYTHON"
-print(my_string3[::-1])
-
-# Lesson2
 #ex_1
 a = 3
 b = a
@@ -27,11 +16,3 @@ print(id(a), id(b), id(c))
 a = 5
 b = a
 print(id(a), id(b))
-#ex_4
-a = input('Введите строку: ')
-my_str1 = a[::2]
-my_str2 = a[1::2]
-print("Введенная строка:", a.strip())
-
-print('Четные символы: ', my_str2)
-print('Нечетные символы: ', my_str1)
