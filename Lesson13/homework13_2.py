9c4c738e7d2b21d3370f50dba12fa95cd66577b9
LobashovaAnastasia <anastasia.lobashova27@gmail.com>
2023-07-04 13:58:33 +0300

    homework13_2

---
+++ Lesson13/homework13_2.py
@@ -0,0 +1,26 @@
+import random
+
+
+class RandomValue:
+    def __init__(self):
+        self._some_list = [el for el in range(1, 101)]
+        self.limit = my_limit
+        self.counter = 0
+
+    def __iter__(self):
+        while self.counter < self.limit:
+            result = self._some_list[random.randint(0, 99)]
+            self.counter += 1
+            yield result
+        print("Iterator is stopped!")
+
+
+my_limit = int(input("Введите лимит: "))
+my_random = RandomValue()
+
+results = [elem for elem in my_random]
+print(results)
+
+assert len(results) == my_limit
+for i in results:
+    assert i is not None
